c5766b25aadf80bf8e66b7b96ce4ffaf8e55c687
crcms <crcms@crcms.cn>
2019-06-29 22:08:26 +0800

    Add comment and format code

--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -15,7 +15,7 @@ type Repository struct {
 	redis  *redis.Client
 }
 
-// 实例化Repository
+// Initialize a new Repository
 func NewRepository(client *redis.Client, prefix string) *Repository {
 
 	if repository != nil {
@@ -32,101 +32,67 @@ func NewRepository(client *redis.Client, prefix string) *Repository {
 	return repository
 }
 
+// Get a warehouse value
 func (this *Repository) Get(key string) (interface{}, error) {
 	return this.redis.Get(this.getPrefix(key)).Result()
 }
 
+// Add a new value to the repository
+// If the key already exists, the addition will be invalid and will not overwrite the original value.
 func (this *Repository) Add(key string, value interface{}, expire time.Time) error {
-
-	//key = this.prefix(key)
-
-	//if this.Has(key) {
-	//	return &cache.Error{RepositoryError: errors.New("exists error")}
-	//}
-
-	//var err error
-	//
-	//if !this.Has(key) {
-	//	 err = this.Put(key, value, expire)
-	//}
-
 	_, err := this.redis.SetNX(this.getPrefix(key), value, expire.Sub(time.Now())).Result()
 
 	return err
 }
 
+// Update a value to the repository
+// If the key already exists, it will automatically overwrite
 func (this *Repository) Put(key string, value interface{}, expire time.Time) error {
-
-	//var err error
-	//key = this.prefix(key)
-
-	//expireDuration :=
-	//// 永久
-	//if expireDuration < 1 {
-	//	expireDuration = 0
-	//}
-
 	_, err := this.redis.Set(this.getPrefix(key), value, expire.Sub(time.Now())).Result()
 
 	return err
-	//
-	//v := reflect.ValueOf(value)
-	//
-	//switch v.Kind() {
-	//case reflect.String:
-	//	this.redis.Set(key, value, expireDuration)
-	//case reflect.Struct, reflect.Map:
-	//	js, err := json.Marshal(value)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	_, err = this.redis.Set(key, string(js), expireDuration).Result()
-	//case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-	//	_, err = this.redis.Set(key, strconv.Itoa(value.(int)), expireDuration).Result()
-	//case reflect.Float64:
-	//	_, err = this.redis.Set(key, strconv.FormatFloat(value.(float64), 'G', 5, 64), expireDuration).Result()
-	//case reflect.Float32:
-	//	_, err = this.redis.Set(key, strconv.FormatFloat(value.(float64), 'G', 5, 32), expireDuration).Result()
-	//default:
-	//	return &cache.Error{RepositoryError: errors.New("type error")}
-	//}
-	//
-	//return err
 }
 
+// Delete an existing key
 func (this *Repository) Forget(key string) error {
 	_, err := this.redis.Del(this.getPrefix(key)).Result()
+
 	return err
 }
 
+// Automatically increments a key by the specified step size
 func (this *Repository) Increment(key string, steps ...int64) error {
 	_, err := this.redis.IncrBy(this.getPrefix(key), this.getStep(steps)).Result()
+
 	return err
 }
 
+// Automatically decrements a key by the specified step size
 func (this *Repository) Decrement(key string, steps ...int64) error {
 	_, err := this.redis.DecrBy(this.getPrefix(key), this.getStep(steps)).Result()
+
 	return err
 }
 
+// Store a permanent valid key
 func (this *Repository) Forever(key string, value interface{}) error {
 	_, err := this.redis.Set(this.getPrefix(key), value, 0).Result()
 
 	return err
 }
 
-// has
+// Determine if a key exists
 func (this *Repository) Has(key string) bool {
 	return this.redis.Exists(this.getPrefix(key)).Val() > 0
 }
 
-// flush all
+// Clear the current entire database
 func (this *Repository) Flush() error {
 	_, err := this.redis.FlushDB().Result()
 	return err
 }
 
-// 数值步长设置
+// Get a specified step size
 func (this *Repository) getStep(steps []int64) int64 {
 	if len(steps) == 0 {
 		return 1
@@ -135,8 +101,7 @@ func (this *Repository) getStep(steps []int64) int64 {
 	}
 }
 
-// prefix
+// Get prefix
 func (this *Repository) getPrefix(key string) string {
-	//return this.prefix + ':' + key
 	return strings.Join([]string{this.prefix, key}, `:`)
 }
