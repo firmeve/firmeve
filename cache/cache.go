ee6a6bb55dce165497a82471bace25d0d3a88b0f
crcms <crcms@crcms.cn>
2019-06-29 22:24:50 +0800

    Add cache repository

--- cache/cache.go
+++ cache/cache.go
@@ -1,14 +1,10 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
-type Error struct {
-	RepositoryError error
-}
-
-func (this *Error) Error() string {
-	return this.RepositoryError.Error()
-}
+var repository *Repository
 
 type Cache interface {
 	Get(key string) (interface{}, error)
@@ -17,22 +13,39 @@ type Cache interface {
 
 	Put(key string, value interface{}, expire time.Time) error
 
+	Forever(key string, value interface{}) error
+
 	Forget(key string) error
 
 	Increment(key string, steps ...int64) error
 
 	Decrement(key string, steps ...int64) error
 
-	Forever(key string, value interface{}) error
-
 	Has(key string) bool
 
 	Flush() error
 }
 
-type Repository struct {
+type Serialization interface {
+	AddEncode(key string, value interface{}, expire time.Time) error
+	ForeverEncode(key string, value interface{}, expire time.Time) error
+	PutEncode(key string, value interface{}, expire time.Time) error
+	GetDecode(key string) (interface{}, error)
 }
 
-func NewRepository() {
+type Repository struct {
+	repositories map[string]Repository
+}
 
+func (this *Repository) NewRepository() *Repository {
+	repository = &Repository{}
+	return repository
+}
+
+type Error struct {
+	RepositoryError error
+}
+
+func (this *Error) Error() string {
+	return this.RepositoryError.Error()
 }
