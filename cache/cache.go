c8705b0396662fe42ad0e06b75568d021e1b5676
crcms <crcms@crcms.cn>
2019-06-30 15:34:36 +0800

    Base cache repository

--- cache/cache.go
+++ cache/cache.go
@@ -1,10 +1,14 @@
 package cache
 
 import (
+	"sync"
 	"time"
 )
 
-var repository *Repository
+var (
+	//repository *Repository
+	once sync.Once
+)
 
 type Cache interface {
 	Get(key string) (interface{}, error)
@@ -27,20 +31,88 @@ type Cache interface {
 }
 
 type Serialization interface {
-	AddEncode(key string, value interface{}, expire time.Time) error
-	ForeverEncode(key string, value interface{}, expire time.Time) error
-	PutEncode(key string, value interface{}, expire time.Time) error
 	GetDecode(key string) (interface{}, error)
+
+	AddEncode(key string, value interface{}, expire time.Time) error
+
+	ForeverEncode(key string, value interface{}, expire time.Time) error
+
+	PutEncode(key string, value interface{}, expire time.Time) error
 }
 
 type Repository struct {
-	repositories map[string]Repository
+	store Cache
 }
 
-func (this *Repository) NewRepository() *Repository {
-	repository = &Repository{}
-	return repository
+func NewRepository(store Cache) *Repository {
+	return &Repository{
+		store: store,
+	}
+	//if repository != nil {
+	//	return repository
+	//}
+	//
+	//once.Do(func() {
+	//	repository = &Repository{
+	//		store,
+	//	}
+	//})
+	//
+	//return repository
 }
+
+func (this *Repository) Get(key string, defaultValue interface{}) (interface{}, error) {
+	if !this.store.Has(key) {
+		return defaultValue, nil
+	}
+
+	return this.store.Get(key)
+}
+
+func (this *Repository) Add(key string, value interface{}, expire time.Time) error {
+	return this.store.Add(key, value, expire)
+}
+
+func (this *Repository) Put(key string, value interface{}, expire time.Time) error {
+	return this.store.Put(key, value, expire)
+}
+
+func (this *Repository) Forever(key string, value interface{}) error {
+	return this.store.Forever(key, value)
+}
+
+func (this *Repository) Forget(key string) error {
+	return this.store.Forget(key)
+}
+
+func (this *Repository) Increment(key string, steps ...int64) error {
+	return this.store.Increment(key, steps...)
+}
+
+func (this *Repository) Decrement(key string, steps ...int64) error {
+	return this.store.Decrement(key, steps...)
+}
+
+func (this *Repository) Has(key string) bool {
+	return this.store.Has(key)
+}
+
+func (this *Repository) Flush() error {
+	return this.store.Flush()
+}
+
+func (this *Repository) Pull(key string, defaultValue interface{}) (interface{}, error) {
+	value, err := this.Get(key, defaultValue)
+	if err != nil {
+		return nil, err
+	}
+
+	return value, this.Forget(key)
+}
+
+//func (this *Repository) drive(driver string) *Repository {
+//
+//}
 
 type Error struct {
 	RepositoryError error
---
+++ cache/cache_test.go
@@ -0,0 +1,132 @@
+package cache
+
+import (
+	"firmeve/cache/redis"
+	"fmt"
+	goRedis "github.com/go-redis/redis"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestNewRepository(t *testing.T) {
+
+	redisStore := redis.NewRepository(goRedis.NewClient(&goRedis.Options{
+		Addr: "192.168.1.107:6379",
+		DB:   0,
+	}), "redis")
+
+	repository := NewRepository(redisStore)
+	fmt.Println(repository)
+	//assert.IsType(t, Cache, repository)
+}
+
+func TestRepository_Get(t *testing.T) {
+	redisRepository := redisRepository()
+
+	key := randString(30)
+
+	value ,err := redisRepository.Get(key,"abc")
+	if err != nil{
+		t.Fail()
+	}
+
+	if value.(string) != "abc" {
+		t.Fail()
+	}
+
+	err = redisRepository.Put(key,"def",time.Now().Add(time.Second * 50))
+	if err != nil{
+		t.Fail()
+	}
+
+	value ,err = redisRepository.Get(key,"abc")
+	if err != nil{
+		t.Fail()
+	}
+
+	if value.(string) != "def" {
+		t.Fail()
+	}
+}
+
+func TestRepository_Pull(t *testing.T) {
+	redisRepository := redisRepository()
+
+	key := randString(30)
+
+	err := redisRepository.Put(key,"def",time.Now().Add(time.Second * 50))
+	if err != nil{
+		t.Fail()
+	}
+
+	value ,err := redisRepository.Pull(key,"abc")
+	if value.(string) != "def" {
+		t.Fail()
+	}
+
+	value ,err = redisRepository.Pull(key,"abc")
+	fmt.Println(value.(string))
+	if value.(string) != "abc" {
+		t.Fail()
+	}
+
+	value ,err = redisRepository.Pull(randString(20),"abc")
+	if value.(string) != "abc" {
+		t.Fail()
+	}
+}
+
+func TestRepository_Forget(t *testing.T) {
+	redisRepository := redisRepository()
+	expire := time.Now().Add(time.Second * 10)
+
+	key := randString(50)
+	err := redisRepository.Add(key, "a", expire)
+	if err != nil {
+		t.Fail()
+	}
+
+	err = redisRepository.Forget(key)
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, false, redisRepository.Has(key))
+}
+
+func TestRepository_Flush(t *testing.T) {
+	redisRepository := redisRepository()
+
+	_ = redisRepository.Put("abc","1",time.Now().Add(time.Hour))
+
+	err := redisRepository.Flush()
+	if err != nil{
+		t.Fail()
+	}
+
+	result := redisRepository.Has("abc")
+	if result{
+		t.Fail()
+	}
+}
+
+// RandString 生成随机字符串
+func randString(len int) string {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		b := r.Intn(26) + 65
+		bytes[i] = byte(b)
+	}
+	return string(bytes)
+}
+
+func redisRepository() *Repository {
+	redisStore := redis.NewRepository(goRedis.NewClient(&goRedis.Options{
+		Addr: "192.168.1.107:6379",
+		DB:   0,
+	}), "redis")
+	return NewRepository(redisStore)
+}
