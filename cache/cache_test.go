fee7159ddc6c2a0ee322c0cdadc6bcc85d2d8ef7
crcms <crcms@crcms.cn>
2019-06-30 18:00:27 +0800

    Cache manager and testing

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"firmeve/cache/redis"
+	config2 "firmeve/config"
 	"fmt"
 	goRedis "github.com/go-redis/redis"
 	"github.com/stretchr/testify/assert"
@@ -27,8 +28,8 @@ func TestRepository_Get(t *testing.T) {
 
 	key := randString(30)
 
-	value ,err := redisRepository.Get(key,"abc")
-	if err != nil{
+	value, err := redisRepository.Get(key, "abc")
+	if err != nil {
 		t.Fail()
 	}
 
@@ -36,13 +37,13 @@ func TestRepository_Get(t *testing.T) {
 		t.Fail()
 	}
 
-	err = redisRepository.Put(key,"def",time.Now().Add(time.Second * 50))
-	if err != nil{
+	err = redisRepository.Put(key, "def", time.Now().Add(time.Second*50))
+	if err != nil {
 		t.Fail()
 	}
 
-	value ,err = redisRepository.Get(key,"abc")
-	if err != nil{
+	value, err = redisRepository.Get(key, "abc")
+	if err != nil {
 		t.Fail()
 	}
 
@@ -56,23 +57,23 @@ func TestRepository_Pull(t *testing.T) {
 
 	key := randString(30)
 
-	err := redisRepository.Put(key,"def",time.Now().Add(time.Second * 50))
-	if err != nil{
+	err := redisRepository.Put(key, "def", time.Now().Add(time.Second*50))
+	if err != nil {
 		t.Fail()
 	}
 
-	value ,err := redisRepository.Pull(key,"abc")
+	value, err := redisRepository.Pull(key, "abc")
 	if value.(string) != "def" {
 		t.Fail()
 	}
 
-	value ,err = redisRepository.Pull(key,"abc")
+	value, err = redisRepository.Pull(key, "abc")
 	fmt.Println(value.(string))
 	if value.(string) != "abc" {
 		t.Fail()
 	}
 
-	value ,err = redisRepository.Pull(randString(20),"abc")
+	value, err = redisRepository.Pull(randString(20), "abc")
 	if value.(string) != "abc" {
 		t.Fail()
 	}
@@ -99,15 +100,15 @@ func TestRepository_Forget(t *testing.T) {
 func TestRepository_Flush(t *testing.T) {
 	redisRepository := redisRepository()
 
-	_ = redisRepository.Put("abc","1",time.Now().Add(time.Hour))
+	_ = redisRepository.Put("abc", "1", time.Now().Add(time.Hour))
 
 	err := redisRepository.Flush()
-	if err != nil{
+	if err != nil {
 		t.Fail()
 	}
 
 	result := redisRepository.Has("abc")
-	if result{
+	if result {
 		t.Fail()
 	}
 }
@@ -129,4 +130,27 @@ func redisRepository() *Repository {
 		DB:   0,
 	}), "redis")
 	return NewRepository(redisStore)
+}
+
+// -------------------- manager ---------------------------
+
+func TestNewManager(t *testing.T) {
+	config, err := config2.NewConfig("./testdata/conf")
+	if err != nil {
+		t.Fail()
+	}
+	var managern *Manager
+	manager := NewManager(config)
+
+	for i := 0; i < 1000; i++ {
+		go func(config *config2.Config) {
+			managern = NewManager(config)
+		}(config)
+	}
+
+	if manager != managern{
+		t.Fail()
+	}
+	//fmt.Printf("%#v", manager)
+
 }
