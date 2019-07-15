046dda7771316da1bd7e40d5608191623ecf4c4f
crcms <crcms@crcms.cn>
2019-07-15 22:57:55 +0800

    testing

--- firmeve.go
+++ firmeve.go
@@ -1,6 +1,8 @@
 package firmeve
 
 import (
+	"fmt"
+	"go/importer"
 	"reflect"
 	"strings"
 	"sync"
@@ -19,7 +21,8 @@ type Container interface {
 
 type binding struct {
 	shared bool
-	object interface{}
+	instance interface{}
+	object func() interface{}
 }
 
 type Firmeve struct {
@@ -41,12 +44,12 @@ func NewFirmeve() *Firmeve {
 	return firmeve
 }
 
-func (f *Firmeve) Bind(id interface{}) { //, value interface{}
+func (f *Firmeve) Bind(object func() interface{},share bool) { //, value interface{}
 	//f.bindings[id] = newBinding(false, value)
-	reflectType := reflect.TypeOf(id)
+	reflectType := reflect.TypeOf(object)
 	if reflectType.Kind() == reflect.Ptr {
 		fullName := strings.Join([]string{reflectType.Elem().PkgPath(), reflectType.Elem().Name()}, ".")
-		f.bindings[fullName] = newBinding(true, id)
+		f.bindings[fullName] = newBinding(share, object)
 	}
 
 }
@@ -54,8 +57,48 @@ func (f *Firmeve) Bind(id interface{}) { //, value interface{}
 func (f *Firmeve) Resolve(id interface{}) interface{}{
 	reflectType := reflect.TypeOf(id)
 	if reflectType.Kind() == reflect.Func {
-		fullName := strings.Join([]string{reflectType.In(0).Elem().PkgPath(), reflectType.In(0).Elem().Name()}, ".")
-		return reflect.ValueOf(id).Call([]reflect.Value{reflect.ValueOf(f.bindings[fullName].object)})[0].Interface()
+
+		//params1Type := reflectType.In(0)//.Elem().Name()
+		//z := nil
+		////params1Value := &params1Type//reflect.NewAt(params1Type,unsafe.Pointer(params1Type.))
+		//fmt.Printf("%#v",reflect.ValueOf(z).Convert(params1Type))
+
+		// 重新设置一个新的struct对象，不过是nil
+		//newObjPtr := reflect.New(params1Type)
+		//newObj := reflect.Indirect(newObjPtr)
+
+		//importer.Default()
+		//fmt.Println(params1Type.Elem().PkgPath())
+		pkg,err := importer.Default().Import(`github.com/firmeve/firmeve/demo`)
+		if err != nil{
+			fmt.Println(err.Error())
+		}
+
+		for _, declName := range pkg.Scope().Names() {
+			fmt.Println(declName)
+		}
+
+
+		//println(params1Type,reflect.ValueOf(params1Value).Interface().(*T1))
+
+
+
+
+		//fullName := strings.Join([]string{reflectType.In(0).Elem().PkgPath(), reflectType.In(0).Elem().Name()}, ".")
+		//fmt.Println(fullName)
+		//fmt.Printf("%#v",reflectType.In(0).Field(0))
+		//stk := reflectType.In(0)
+		//stkValue := reflect.ValueOf(reflect.TypeOf(id).In(0))
+		////reflect.New(stk)
+		////fmt.Printf("%#v",stkValue.MethodByName("NewT1").Call(make([]reflect.Value,0))[0].Interface())
+		//fmt.Printf("%#v\n",stk.Elem().PkgPath())
+		////zx := reflect.ValueOf(NewT1())
+		//stkValue.Elem().Set(reflect.New(stk))
+		//reflect.NewAt(stk, unsafe.Pointer(stk.UnsafeAddr())).Elem()
+		//fmt.Printf("%#v",stkValue.Interface())
+		//fmt.Printf("%#v",stkValue.Call([]reflect.Value{reflect.ValueOf("NewT1")}))
+
+		//return reflect.ValueOf(id).Call([]reflect.Value{reflect.ValueOf(f.bindings[fullName].object)})[0].Interface()
 	}
 
 	return  nil
--- firmeve_test.go
+++ firmeve_test.go
@@ -1,32 +1,18 @@
 package firmeve
 
 import (
-	"fmt"
 	"testing"
 )
-
-type T1 struct {
-	Name string
-}
-
-func NewT1() *T1 {
-	return &T1{"Simon"}
-}
-
-type T2 struct {
-	t1 *T1
-	Age int
-}
-
-func NewT2(f *T1) T2  {
-	return T2{t1:f,Age:10}
-}
 
 
 func TestFirmeve_Bind(t *testing.T) {
 	firmeve := NewFirmeve()
-	t1 := NewT1()
-	firmeve.Bind(t1)
+	//t1 := NewT1()
+	//firmeve.Bind(t1)
 
-	fmt.Printf("%#v",firmeve.Resolve(NewT2).(T2))
+	firmeve.Bind(func() interface{} {
+		return NewT1()
+	},false)
+
+	//fmt.Printf("%#v",firmeve.Resolve(demo.NewT2).(T2))
 }
