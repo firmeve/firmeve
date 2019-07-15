bd76a69e1ac90c810968fddf19b14dc9a3960e9e
crcms <crcms@crcms.cn>
2019-07-15 23:16:49 +0800

    Description

--- firmeve.go
+++ firmeve.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"go/importer"
 	"reflect"
-	"strings"
 	"sync"
 )
 
@@ -20,9 +19,9 @@ type Container interface {
 }
 
 type binding struct {
-	shared bool
+	shared   bool
 	instance interface{}
-	object func() interface{}
+	object   func() interface{}
 }
 
 type Firmeve struct {
@@ -44,18 +43,44 @@ func NewFirmeve() *Firmeve {
 	return firmeve
 }
 
-func (f *Firmeve) Bind(object func() interface{},share bool) { //, value interface{}
+// 0. 每一个使用的都必须要注册，但可以不是单例，在init函数中是个好的选择
+// 1. 全部存储函数类型
+// 2. 如果是singleton保存到instance实例
+// 3. 先实现这样的bind和resolve，后面增加struct tag 注入
+
+func (f *Firmeve) Bind(object interface{}, share bool) { //, value interface{}
 	//f.bindings[id] = newBinding(false, value)
 	reflectType := reflect.TypeOf(object)
-	if reflectType.Kind() == reflect.Ptr {
-		fullName := strings.Join([]string{reflectType.Elem().PkgPath(), reflectType.Elem().Name()}, ".")
-		f.bindings[fullName] = newBinding(share, object)
-	}
+	println(reflectType.Kind().String())
+	//if reflectType.Kind() == reflect.Ptr {
+	//	fullName := strings.Join([]string{reflectType.Elem().PkgPath(), reflectType.Elem().Name()}, ".")
+	//	f.bindings[fullName] = newBinding(share, object)
+	//}
 
+	//var method func(f *Firmeve) interface{}
+	//
+	//if reflectType.Kind() != reflect.Func {
+	//	method = func(f *Firmeve) interface{} {
+	//		return object
+	//	}
+	//} else {
+	//	method = func(f *Firmeve) interface{} {
+	//		reflect.ValueOf(object).Elem().Call()
+	//	}
+	//}
+
+
+
+	if share {
+
+	} else {
+
+	}
 }
 
-func (f *Firmeve) Resolve(id interface{}) interface{}{
+func (f *Firmeve) Resolve(id interface{}) interface{} {
 	reflectType := reflect.TypeOf(id)
+
 	if reflectType.Kind() == reflect.Func {
 
 		//params1Type := reflectType.In(0)//.Elem().Name()
@@ -69,8 +94,8 @@ func (f *Firmeve) Resolve(id interface{}) interface{}{
 
 		//importer.Default()
 		//fmt.Println(params1Type.Elem().PkgPath())
-		pkg,err := importer.Default().Import(`github.com/firmeve/firmeve/demo`)
-		if err != nil{
+		pkg, err := importer.Default().Import(`github.com/firmeve/firmeve/demo`)
+		if err != nil {
 			fmt.Println(err.Error())
 		}
 
@@ -78,11 +103,7 @@ func (f *Firmeve) Resolve(id interface{}) interface{}{
 			fmt.Println(declName)
 		}
 
-
 		//println(params1Type,reflect.ValueOf(params1Value).Interface().(*T1))
-
-
-
 
 		//fullName := strings.Join([]string{reflectType.In(0).Elem().PkgPath(), reflectType.In(0).Elem().Name()}, ".")
 		//fmt.Println(fullName)
@@ -101,14 +122,15 @@ func (f *Firmeve) Resolve(id interface{}) interface{}{
 		//return reflect.ValueOf(id).Call([]reflect.Value{reflect.ValueOf(f.bindings[fullName].object)})[0].Interface()
 	}
 
-	return  nil
+	return nil
 }
 
 // ---------------------------- binding ------------------------
 
-func newBinding(shared bool, object interface{}) *binding {
+func newBinding(shared bool, object func() interface{}) *binding {
 	return &binding{
-		shared: shared,
-		object: object,
+		shared:   shared,
+		object:   object,
+		instance: nil,
 	}
 }
