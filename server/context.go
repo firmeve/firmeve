e5ffeed178cebdb16d0ef9e8060f495a9649891f
simon <simon@crcms.cn>
2019-07-25 16:07:48 +0800

    Base context testing

--- server/context.go
+++ server/context.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"context"
+	"time"
+)
+
+type Context struct {
+	protocol context.Context
+}
+
+func (c *Context) Deadline() (deadline time.Time, ok bool) {
+	panic("implement me")
+}
+
+func (c *Context) Done() <-chan struct{} {
+	return nil
+}
+
+func (c *Context) Err() error {
+	return nil
+}
+
+func (c *Context) Value(key interface{}) interface{} {
+	panic("implement me")
+}
