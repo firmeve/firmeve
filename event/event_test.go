8524f86bf0cc7e1873576bac7c60d8f19349462e
crcms <crcms@crcms.cn>
2019-10-30 22:04:54 +0800

    Merge event testing

--- event/event_test.go
+++ event/event_test.go
@@ -1,6 +1,7 @@
 package event
 
 import (
+	firmeve2 "github.com/firmeve/firmeve"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -49,3 +50,13 @@ func TestNamePanic(t *testing.T)  {
 		dispatch.dispatch("c", listenBOne)
 	},`the event not exists`)
 }
+
+
+func TestProvider_Register(t *testing.T) {
+	firmeve := firmeve2.NewFirmeve()
+	firmeve.Boot()
+	assert.Equal(t, true, firmeve.HasProvider("event"))
+	assert.Equal(t,true,firmeve.Has(`event`))
+}
+
+
