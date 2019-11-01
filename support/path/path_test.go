c07ef4a46749226a16374137db23f6b36cf36813
simon <simon@crcms.cn>
2019-11-01 16:12:11 +0800

    Path testing

---
+++ support/path/path_test.go
@@ -0,0 +1,19 @@
+package path
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRunFile(t *testing.T) {
+	assert.Contains(t, RunFile(), "path_test")
+}
+
+func TestRunDir(t *testing.T) {
+	assert.Contains(t, RunDir(), "support/path")
+}
+
+func TestRunRelative(t *testing.T) {
+	assert.Contains(t, RunRelative("../"), "support")
+	assert.NotContains(t, RunRelative("../"), "path")
+}
