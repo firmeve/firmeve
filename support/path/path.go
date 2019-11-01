a95238e637d971351ad1d1fb9667305642f5246c
simon <simon@crcms.cn>
2019-11-01 15:37:18 +0800

    Add path support

---
+++ support/path/path.go
@@ -0,0 +1,38 @@
+package path
+
+import (
+	"path"
+	"path/filepath"
+	"runtime"
+)
+
+func callerStepFile(skip int) string {
+	_, file, _, ok := runtime.Caller(skip)
+	if !ok {
+		panic(`Can not get current file info`)
+	}
+
+	return file
+}
+
+// Get current running file path
+func RunFile() string {
+	// 0 is current file, so except
+	// 1 is current file, so except
+	return callerStepFile(2)
+}
+
+// Get current running directory path
+func RunDir() string {
+	return path.Dir(callerStepFile(2))
+}
+
+// Get current directory relative path
+func RunRelative(rpath string) string {
+	rpath, err := filepath.Abs(filepath.Join(path.Dir(callerStepFile(2)), rpath))
+	if err != nil {
+		panic(err)
+	}
+
+	return rpath
+}
