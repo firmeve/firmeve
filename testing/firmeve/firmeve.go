d57d50125b60bce0ac7375b4b4d8e86565ffcd5f
simon <simon@crcms.cn>
2019-11-22 10:04:35 +0800

    Optimization test start

--- testing/firmeve/firmeve.go
+++ testing/firmeve/firmeve.go
@@ -3,14 +3,11 @@ package firmeve
 import (
 	"github.com/firmeve/firmeve"
 	bootstrap2 "github.com/firmeve/firmeve/bootstrap"
+	"github.com/firmeve/firmeve/support"
 )
 
-func Bootstrap(configPath string) *firmeve.Firmeve {
-	f := firmeve.New()
-
-	bootstrap := bootstrap2.New(f, configPath)
-	bootstrap.RegisterDefault()
-	bootstrap.Boot()
-
+func Bootstrap(options ...support.Option) *firmeve.Firmeve {
+	f := firmeve.New(firmeve.WithMode(firmeve.ModeTesting))
+	bootstrap2.New(f, options...).FastBootFull()
 	return f
 }
