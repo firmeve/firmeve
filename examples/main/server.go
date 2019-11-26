46e794d1de679f95fa3cc0999b5212c81bca5705
simon <simon@crcms.cn>
2019-11-26 12:26:41 +0800

    Server demo change

--- examples/main/server.go
+++ examples/main/server.go
@@ -1,148 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"os"
-
 	path2 "github.com/firmeve/firmeve/support/path"
-
 	"github.com/firmeve/firmeve/bootstrap"
-
 	cmd2 "github.com/firmeve/firmeve/http/cmd"
-
-	"github.com/firmeve/firmeve/converter/resource"
-
 	"github.com/firmeve/firmeve"
-
 	"github.com/firmeve/firmeve/cmd"
-	"github.com/firmeve/firmeve/converter/transform"
 	"github.com/firmeve/firmeve/http"
 	_ "github.com/takama/daemon"
 )
 
-type (
-	Something struct {
-		Id    int    `json:"id"`
-		Title string `json:"title"`
-	}
-
-	Original struct {
-		Id    int
-		Title string
-		Name  string
-	}
-
-	AnyTransformer struct {
-		transform.BaseTransformer
-	}
-)
-
-func (s *Something) SetId() {
-	s.Id += 1
-}
-
-func (a *AnyTransformer) IdField() int {
-	return a.Resource().(*Original).Id * 10
-}
-
-func router(router *http.Router) *http.Router {
-	router.GET(`/item`, func(c *http.Context) {
-		c.Item(&Original{
-			10,
-			"Title",
-			"Simon",
-		}, &resource.Option{
-			Fields:      []string{"id", "title", "name"},
-			Transformer: new(AnyTransformer),
-		})
-		c.Next()
-	})
-	router.GET(`/collection`, func(c *http.Context) {
-		z := []*Original{
-			&Original{
-				10, "title", "simon",
-			},
-			&Original{
-				12, "title", "simon",
-			},
-			&Original{
-				13, "title", "simon",
-			},
-			&Original{
-				14, "title", "simon",
-			},
-			&Original{
-				15, "title", "simon",
-			},
-		}
-		c.Collection(z, &resource.Option{
-			Transformer: new(AnyTransformer),
-			Fields:      []string{"id", "title"},
-		})
-		c.Next()
-	})
-	router.GET("/abc", func(ctx *http.Context) {
-		//time.Sleep(time.Second * 10)
-		//ctx.String("abc")
-		//s := ctx.Firmeve.Get("something").(*Something)
-		//s.SetId()
-		//fmt.Printf("%#v\n", s)
-		//ctx.Json(s)
-		s := ctx.Firmeve.Make(new(Something)).(*Something)
-		s.SetId()
-		fmt.Printf("%#v\n", s)
-		ctx.JSON(s)
-		//if !ctx.Firmeve.Has("something") {
-		//	ctx.Firmeve.Bind("something", ctx.Firmeve.Make(new(Something)))
-		//	ctx.Json(Something{
-		//		Id:    0,
-		//		Title: "",
-		//	})
-		//} else {
-		//	s := ctx.Firmeve.Get("something").(*Something)
-		//	s.SetId()
-		//	fmt.Printf("%#v\n", s)
-		//	ctx.Json(s)
-		//}
-
-		ctx.Next()
-	}).After(func(ctx *http.Context) {
-		//ctx.Json(Something{
-		//	Id:    10,
-		//	Title: "title",
-		//})
-		ctx.Next()
-	})
-	return router
-}
-
-type Testing struct {
+type App struct {
 	firmeve.BaseProvider
 }
 
-func (t *Testing) Name() string {
-	return `testing`
+func (a *App) Name() string {
+	return `app`
 }
 
-func (t *Testing) Register() {
-	router(t.Firmeve.Get(`http.router`).(*http.Router))
+func (a *App) Register() {
 }
 
-func (t *Testing) Boot() {
+func (a *App) Boot() {
+	a.bindingRoutes()
+}
+
+func (a *App) bindingRoutes() {
+	router := a.Firmeve.Get(`http.router`).(*http.Router)
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET(`/ping`, func(c *http.Context) {
+			c.Data(map[string]string{
+				"message": "pong",
+			})
+			c.Next()
+		})
+	}
 }
 
 func main() {
+	// Base bootstrap
 	app := firmeve.New()
-
-	app.Bind("something", func() *Something {
-		return app.Make(new(Something)).(*Something)
-	})
-
 	bootstrap2 := bootstrap.New(app, bootstrap.WithConfigPath(path2.RunRelative(`../../testdata/config`)))
 	bootstrap2.FastBootFullWithProviders(
-		[]firmeve.Provider{new(Testing)},
+		[]firmeve.Provider{new(App)},
 	)
-	//
+
+	// Command
 	root := cmd.Root()
 	root.AddCommand(cmd2.NewServer(bootstrap2).Cmd())
 	root.SetArgs(os.Args[1:])
