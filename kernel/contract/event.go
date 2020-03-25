ec3a6bdf47073cc4c801948739d87873f8e2a62d
simon <simon@crcms.cn>
2020-03-25 16:17:52 +0800

    Event shortcut parameters

--- kernel/contract/event.go
+++ kernel/contract/event.go
@@ -1,14 +1,16 @@
 package contract
 
 type (
+	EventInParams map[string]interface{}
+
 	EventHandler interface {
-		Handle(params map[string]interface{}) (interface{}, error)
+		Handle(params EventInParams) (interface{}, error)
 	}
 
 	Event interface {
 		Listen(name string, handler EventHandler)
 		ListenMany(name string, handlers []EventHandler)
-		Dispatch(name string, params map[string]interface{}) []interface{}
+		Dispatch(name string, params EventInParams) []interface{}
 		Has(name string) bool
 	}
 )
