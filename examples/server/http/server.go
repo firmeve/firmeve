58c76ff589e68fc3ab599f2af78c8f1c2370c68b
simon <simon@crcms.cn>
2020-04-17 10:56:46 +0800

    pprof

--- examples/server/http/server.go
+++ examples/server/http/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/firmeve/firmeve/kernel"
 	"github.com/firmeve/firmeve/kernel/contract"
 	"github.com/firmeve/firmeve/render"
+	"net/http/pprof"
 )
 
 type App struct {
@@ -28,6 +29,14 @@ func (a *App) Boot() {
 			})
 			c.Next()
 		})
+	}
+	debug := router.Group("/debug")
+	{
+		debug.Handler("GET", "/pprof", pprof.Index)
+		debug.Handler("GET", "/cmdline", pprof.Cmdline)
+		debug.Handler("GET", "/profile", pprof.Profile)
+		debug.Handler("GET", "/symbol", pprof.Symbol)
+		debug.Handler("GET", "/trace", pprof.Trace)
 	}
 }
 
