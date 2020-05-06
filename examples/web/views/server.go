50d2733fd72007537c8cfce4a4c8c234cf698f33
simon <simon@crcms.cn>
2020-05-06 15:03:17 +0800

    Example views

---
+++ examples/web/views/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/firmeve/firmeve"
+	"github.com/firmeve/firmeve/kernel"
+	"github.com/firmeve/firmeve/kernel/contract"
+	"github.com/firmeve/firmeve/render"
+)
+
+type App struct {
+	kernel.BaseProvider
+}
+
+func (a *App) Name() string {
+	return `app`
+}
+
+func (a *App) Register() {
+}
+
+func (a *App) Boot() {
+	router := a.Firmeve.Get(`http.router`).(contract.HttpRouter)
+	web := router.Group("")
+	{
+		web.GET("/", func(c contract.Context) {
+			c.RenderWith(200, render.Html, render.Template{
+				Name:   "index",
+				Data:   "海河小王子",
+				Append: nil,
+			})
+			c.Next()
+		})
+	}
+}
+
+func main() {
+	firmeve.RunDefault(firmeve.WithProviders(
+		[]contract.Provider{
+			new(App),
+		},
+	))
+}
