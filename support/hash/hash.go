30350af5669451910f72f004c22f5974f6498401
simon <simon@crcms.cn>
2020-05-07 22:32:48 +0800

    Add md5 and sha256 hash

--- support/hash/hash.go
+++ support/hash/hash.go
@@ -1,18 +1,47 @@
 package hash
 
 import (
+	"crypto/md5"
 	sha12 "crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 )
+
+var (
+	sha1Hash   = sha12.New()
+	md5Hash    = md5.New()
+	sha256Hash = sha256.New()
+)
+
+func Sha1(target string) []byte {
+	sha1Hash.Write([]byte(target))
+	hash := sha1Hash.Sum(nil)
+	sha1Hash.Reset()
+	return hash
+}
 
 func Sha1String(target string) string {
 	return hex.EncodeToString(Sha1(target))
 }
 
-func Sha1(target string) []byte {
-	sha1Hash := sha12.New()
-	sha1Hash.Write([]byte(target))
-	hash := sha1Hash.Sum(nil)
-	sha1Hash.Reset()
+func Md5(target string) []byte {
+	md5Hash.Write([]byte(target))
+	hash := md5Hash.Sum(nil)
+	md5Hash.Reset()
 	return hash
+}
+
+func Md5String(target string) string {
+	return hex.EncodeToString(Md5(target))
+}
+
+func Sha256(target string) []byte {
+	sha256Hash.Write([]byte(target))
+	hash := sha256Hash.Sum(nil)
+	sha256Hash.Reset()
+	return hash
+}
+
+func Sha256String(target string) string {
+	return hex.EncodeToString(Sha256(target))
 }
