150d26fa4772c5f5defe38575cc8d21f4b2a531d
simon <simon@crcms.cn>
2020-05-10 17:40:33 +0800

    Revert "Perform position adjustment"
    This reverts commit 224671ab57c6662f9a8e33c77fa4719708359e86.

--- kernel/command.go
+++ kernel/command.go
@@ -41,6 +41,11 @@ func (c *command) AddCommand(commands ...contract.Command) {
 			// bootstrap
 			c.boot(configPath, devModeBool)
 
+			// mount
+			if c.mount != nil {
+				c.mount(c.Application())
+			}
+
 			// panic handler
 			defer Recover(c.Resolve(`logger`).(contract.Loggable))
 
@@ -63,11 +68,6 @@ func (c *command) boot(configPath string, devMode bool) {
 	c.firmeve.Bind(`firmeve`, c.firmeve)
 	c.firmeve.Bind(`application`, c.firmeve)
 	c.firmeve.Bind(`config`, config.New(configPath), container.WithShare(true))
-
-	// mount
-	if c.mount != nil {
-		c.mount(c.Application())
-	}
 
 	c.firmeve.RegisterMultiple(c.providers, false)
 
