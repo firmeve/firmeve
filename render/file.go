25eb92f83328bec7ccf69f12e125db4cb1d3f063
simon <simon@crcms.cn>
2020-05-15 10:55:06 +0800

    Add render file

---
+++ render/file.go
@@ -0,0 +1,42 @@
+package render
+
+import (
+	"fmt"
+	"github.com/firmeve/firmeve/kernel/contract"
+	"io/ioutil"
+	"os"
+)
+
+type (
+	file struct {
+	}
+
+	FileOption struct {
+		Mime     string
+		Path     string
+		Filename string
+	}
+)
+
+var (
+	File = file{}
+)
+
+func (f file) Render(protocol contract.Protocol, status int, v interface{}) error {
+	httpProtocol := protocol.(contract.HttpProtocol)
+	option := v.(FileOption)
+
+	file, err := os.Open(option.Filename)
+	if err != nil {
+		httpProtocol.ResponseWriter().WriteHeader(404)
+		return err
+	}
+	httpProtocol.ResponseWriter().WriteHeader(status)
+	httpProtocol.SetHeader(`Content-Type`, option.Mime)
+	httpProtocol.SetHeader("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", option.Filename))
+
+	defer file.Close()
+	c, _ := ioutil.ReadAll(file)
+	_, err = httpProtocol.Write(c)
+	return err
+}
