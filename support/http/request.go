ab689673907dedc8ea18aa1178017d4c86d598fa
simon <simon@crcms.cn>
2020-06-02 22:23:25 +0800

    Http request ip

---
+++ support/http/request.go
@@ -0,0 +1,17 @@
+package http
+
+import (
+	"net/http"
+	"strings"
+)
+
+func ClientIP(req *http.Request) string {
+	addr := req.Header.Get("X-Real-IP")
+	if addr == "" {
+		addr = req.Header.Get("X-Forwarded-For")
+		if addr == "" {
+			addr = req.RemoteAddr
+		}
+	}
+	return strings.Split(addr, `:`)[0]
+}
