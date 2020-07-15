b135b6851384b56cd4efcabc01c256a75a3031b0
simon <simon@crcms.cn>
2020-07-15 14:05:50 +0800

    Chaged: http server testing

--- http/server_test.go
+++ http/server_test.go
@@ -1,7 +1,9 @@
 package http
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"time"
 
 	//testing2 "github.com/firmeve/firmeve/testing"
@@ -10,7 +12,15 @@ import (
 	"testing"
 )
 
+func init() {
+	//runtime.GOMAXPROCS(1)
+}
+
+var lock sync.Mutex
+
 func TestNewServer(t *testing.T) {
+	lock.Lock()
+	defer lock.Unlock()
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -18,20 +28,21 @@ func TestNewServer(t *testing.T) {
 	server1 := NewServer(router, map[string]interface{}{
 		`host`: `0.0.0.0:11211`,
 	})
-
-	go func() {
-		err := server1.Start()
-		assert.Nil(t, err)
-	}()
-	time.Sleep(time.Second * 2)
-	err := server1.Stop()
+	ctx := context.Background()
+	go func(t2 *testing.T) {
+		server1.Start(ctx)
+	}(t)
+	time.Sleep(time.Second * 1)
+	err := server1.Stop(ctx)
 	assert.Nil(t, err)
 }
 
 func TestNewServerHttps(t *testing.T) {
+	lock.Lock()
+	defer lock.Unlock()
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-
+	ctx := context.Background()
 	router := mock.NewMockHttpRouter(mockCtrl)
 	server1 := NewServer(router, map[string]interface{}{
 		`host`:      `0.0.0.0:11222`,
@@ -39,19 +50,21 @@ func TestNewServerHttps(t *testing.T) {
 		`key-file`:  `../testdata/ssl/server.key`,
 	})
 
-	go func() {
-		err := server1.Start()
-		assert.Nil(t, err)
-	}()
+	go func(t2 *testing.T) {
+		server1.Start(ctx)
+		//assert.Nil(t2, err)
+	}(t)
 	time.Sleep(time.Second * 2)
-	err := server1.Stop()
+	err := server1.Stop(ctx)
 	assert.Nil(t, err)
 }
 
 func TestNewServerHttp2(t *testing.T) {
+	lock.Lock()
+	defer lock.Unlock()
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-
+	ctx := context.Background()
 	router := mock.NewMockHttpRouter(mockCtrl)
 	server1 := NewServer(router, map[string]interface{}{
 		`host`:      `0.0.0.0:11223`,
@@ -60,11 +73,10 @@ func TestNewServerHttp2(t *testing.T) {
 		`http2`:     true,
 	})
 
-	go func() {
-		err := server1.Start()
-		assert.Nil(t, err)
-	}()
+	go func(t2 *testing.T) {
+		server1.Start(ctx)
+	}(t)
 	time.Sleep(time.Second * 2)
-	err := server1.Stop()
+	err := server1.Stop(ctx)
 	assert.Nil(t, err)
 }
