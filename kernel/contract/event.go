9e743fe4f70a5f717fee158443685ae4f16106ba
simon <simon@crcms.cn>
2020-08-05 13:12:10 +0800

    Changed: merge listen

--- kernel/contract/event.go
+++ kernel/contract/event.go
@@ -6,8 +6,7 @@ type (
 	}
 
 	Event interface {
-		Listen(name string, handler EventHandler)
-		ListenMany(name string, handlers []EventHandler)
+		Listen(name string, handlers ...EventHandler)
 		Dispatch(name string, params ...interface{}) []interface{}
 		Has(name string) bool
 	}
